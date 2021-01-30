e72f60c7e61b02f7af6ea0914731be28c384e92c
akolatch <aaron.kolatch@gmail.com>
2021-01-30 11:37:51 -0500

    db and schema full set up

---
+++ DB/index.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/QnA');
+
+const questionsSchema = new mongoose.Schema({
+  product_id: { type: Number, index: true },
+  asker_name: String,
+  asker_email: String,
+  reported: Number,
+  question_body: String,
+  question_date: String,
+  question_helpfulness: Number,
+  question_id: { type: Number, index: true },
+});
+
+const answersSchema = new mongoose.Schema({
+  answer_id: { type: Number, index: true },
+  question_id: { type: Number, index: true },
+  body: String,
+  answerer_name: String,
+  answerer_email: String,
+  reported: Number,
+  photos: Array,
+  date: String,
+  helpfulness: Number,
+});
+
+const Question = mongoose.model('Question', questionsSchema, 'questions');
+const Answer = mongoose.model('Answer', answersSchema, 'answers');
+
+module.exports = {
+  Answer,
+  Question,
+};
