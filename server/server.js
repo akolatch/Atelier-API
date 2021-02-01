49e900d9a2b9c55d17246166d132de4aee6d0a1a
akolatch <aaron.kolatch@gmail.com>
2021-02-01 09:46:05 -0500

    remove unused dependencies and morgan for deployment

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 const routes = require('./router.js');
-const morgan = require('morgan');
+// const morgan = require('morgan');
 
-app.use(morgan('dev'));
+// app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/qa', routes);
